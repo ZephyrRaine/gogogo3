a05a4427406f784792567308d74c2a65d68d101f
Tcartegnie <Sorakssis@gmail.com>
2026-01-18 19:02:40 +0100

    Add a particle drop FX

--- game/scripts/water_drop_particle.gd
+++ game/scripts/water_drop_particle.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 class_name water_drop_particle 
 
-@onready
-var UpperDrop :GPUParticles2D = get_node("WaterDropParticle")
+
 @onready
 var LeftDrop :GPUParticles2D = get_node("WaterDropParticleLateralLeft")
 @onready
@@ -12,15 +11,33 @@ var RightDrop :GPUParticles2D = get_node("WaterDropParticleLateralRight")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	EventBus.bounce.connect(_splashParticle)
+	EventBus.launch_done.connect(_unsplashParticle)
 	pass # Replace with function body.
 
 
-func _splashParticle() -> void:
-	UpperDrop.emitting = true;
-	LeftDrop.emitting = true;
-	RightDrop.emitting = true;
-	pass 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _unsplashParticle(distance : float, bounces : float) ->void :
+
 	
+	LeftDrop.emitting = false;
+	
+	RightDrop.emitting = false;
 	pass
+
+func _splashParticle(lucky : bool) -> void:
+
+	LeftDrop.emitting = true;
+	LeftDrop.restart(false);
+	
+	RightDrop.emitting = true;
+	RightDrop.restart(false);
+	pass
+
+
+func _on_water_drop_particle_lateral_right_finished() -> void:
+	LeftDrop.emitting = false;
+	pass # Replace with function body.
+
+
+func _on_water_drop_particle_lateral_left_finished() -> void:
+	RightDrop.emitting = false;
+	pass # Replace with function body.
