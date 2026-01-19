f9028ec7cfdd2f9d760c44b30f974751f421679b
Ambeurk <ambre.lacour@laposte.net>
2026-01-19 15:49:49 +0100

    eagle, holy & dash ricochet

--- game/scripts/main/hud/effects_manager.gd
+++ game/scripts/main/hud/effects_manager.gd
@@ -3,6 +3,8 @@ extends Control
 
 @export var rebound_label_lucky:ReboundLabel
 @export var rebound_label_eagle:ReboundLabel
+@export var rebound_label_holy:ReboundLabel
+@export var rebound_label_dash:ReboundLabel
 var pebble:Pebble
 
 # Called when the node enters the scene tree for the first time.
@@ -15,3 +17,9 @@ func on_bounce(is_lucky:bool):
 		
 	if pebble.current_bounce_is_eagle:
 		rebound_label_eagle.start_anim(pebble.get_global_transform_with_canvas().origin)
+		
+	if pebble.current_bounce_is_holy:
+		rebound_label_holy.start_anim(pebble.get_global_transform_with_canvas().origin)
+		
+	if pebble.current_bounce_is_dash:
+		rebound_label_dash.start_anim(pebble.get_global_transform_with_canvas().origin)
